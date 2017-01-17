bd4a64694cdb2bc27e80458c69ed7f9f8a85ba6f
dhlopez <dhelaman@hotmail.com>
2017-01-17 12:27:03 -0500

    changes for Jan 17

--- Practice1/app.js
+++ Practice1/app.js
@@ -9,14 +9,23 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/', routes.index);
 
+var serve = http.createServer(app); 
+var io = require('socket.io')(serve);
+
 //var port = process.env.port || 1337;
-http.createServer(app).listen(app.get('port'), function () {
+serve.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
